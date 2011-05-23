a30a7a5d72806de6c8e40f97a9d4423c7457c205
forair <azheng270@gmail.com>
2011-05-23 16:36:38 +0800

    ehcache

--- src/common/com/gm/common/cache/Cache.java
+++ src/common/com/gm/common/cache/Cache.java
@@ -1,22 +1,37 @@
 package com.gm.common.cache;
 
+import java.util.List;
 import java.util.concurrent.locks.ReadWriteLock;
 
 public interface Cache {
 
-	String getId();
-
 	int getSize();
 
-	void putObject(Object key, Object value);
+	void set(Object key, Object value);
 
-	Object getObject(Object key);
+	/**
+	 * Replace the cached element only if an Element is currently cached for this key
+	 * @param key
+	 * @param value
+	 */
+	void replace(Object key, Object value);
+
+	Object get(Object key);
 
 	boolean hasKey(Object key);
 
-	Object removeObject(Object key);
+	/**
+	 * true if the element was removed, false if it was not found in the cache
+	 * @param key
+	 * @return
+	 */
+	boolean remove(Object key);
 
 	void clear();
 
-	ReadWriteLock getReadWriteLock();
+	ReadWriteLock getLock();
+
+	List getAll();
+
+	List getByKeys();
 }
